f0624d8f7d6ab46008ea44859c46dbf8a5345189
Gleb Popov <gorlanandroid@gmail.com>
2019-10-12 13:36:18 +0700

    Added file dialog and completed menu

--- bash/lab2/lab2.sh
+++ bash/lab2/lab2.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
-CHOOSED=$(kdialog --menu "Выберите действие:" 1 Добавить 2 Будильник 3 Удалить); 
+while true
+do
+CHOOSED=$(kdialog --menu "Выберите действие:" 1 Добавить 2 Будильник 3 Удалить 4 Выход); 
+if [ $? -eq 1 ]
+then
+	break;
+fi
 case $CHOOSED in
 1)
 TIME=$(kdialog --title "Будильники" --inputbox "Введите время");
@@ -7,20 +13,32 @@ if [ $? -eq 0 ]
 then
 	m=$(echo $TIME | cut -d':' -f2);
 	h=$(echo $TIME | cut -d':' -f1);
-	RESULT="Выберите дни.\n";
-	DATE=$(kdialog --inputbox "Введите дни недели");
+	RESULT="Выберите музыку.\n";
+	MUSIC=$(kdialog --getopenfilename /usr/share/sounds/ '*.ogg');
 	if [ $? -eq 0 ]
 	then
-		crontab -l > temp;
-		echo -e "$m $h * * $DATE  clementine /home/gleb/Linux/bash/lab2/04_Rocket\ Man.wav" >> temp;
-		crontab temp;
-		rm temp;
-		RESULT="Будильник был добавлен.\n";
+		RESULT="Выберите дни.\n";
+		DATE=$(kdialog --inputbox "Введите дни недели");
+		if [ $? -eq 0 ]
+		then
+			if [ $? -eq 0 ]
+			then
+				crontab -l > temp;
+				echo -e "$m $h * * $DATE  clementine $MUSIC" >> temp;
+				crontab temp;
+				rm temp;
+				kdialog --msgbox "Будильник был добавлен.";
+			else
+				kdialog --error "Будильник не был добавлен.";
+			fi
+		else
+			kdialog --error "Будильник не был добавлен.";
+		fi
 	else
-		RESULT="Будильник не был добавлен.\n";
+		kdialog --error "Будильник не был добавлен.";
 	fi
 else
-	RESULT="Будильник не был добавлен.\n";
+	kdialog --error "Будильник не был добавлен.";
 fi
 ;;
 2)
@@ -53,12 +71,16 @@ then
 		crontab newcron;
 		rm temp;
 		rm newcron;
-		RESULT="Будильник удален успешно.\n"
+		kdialog --msgbox "Будильник удален успешно."
 	else
-		RESULT="Будильник не удален.\n"
+		kdialog --error "Будильник не удален."
 	fi
 else
-	RESULT+="Будильник не удален.\n"
+	kdialog --error "Будильник не удален"
 fi
 ;;
+4)
+break;
+;;
 esac
+done
