c28a7fbabf7f69bcb89c2b0e5968c47de464a630
Gleb Popov <gorlanandroid@gmail.com>
2019-11-30 14:28:51 +0700

    Edited lab8

--- C/lab8/lab8.c
+++ C/lab8/lab8.c
@@ -1,11 +1,8 @@
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <wait.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/errno.h>
