cac9871d873473a66696b69c37598639c64fe199
Gleb Popov <gorlanandroid@gmail.com>
2019-12-20 14:17:02 +0700

    Edited client

--- C/lab11/client.c
+++ C/lab11/client.c
@@ -15,15 +15,19 @@ int main(int argc, char *argv[]) {
 
     char buff[1024];
     printf("КЛИЕНТ: начало работы\n");
-    if ((my_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((my_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         perror("Ошибка открытия сокета!\n");
+        exit(-1);
+    }
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(12345);
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-    if (connect(my_sock,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+    if (connect(my_sock,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0){
         perror("Ошибка соединения!\n");
+        exit(-2);
+    }
 
     while ((n = recv(my_sock, &buff[0], sizeof(buff) - 1, 0)) > 0) {
         buff[n] = '\0';
