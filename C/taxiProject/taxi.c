f80dde194cc74fe471c064cec1aa67085c423959
Gleb Popov <gorlanandroid@gmail.com>
2020-09-27 01:04:37 +0700

    Add multi-thread version of taxiProject

---
+++ C/taxiProject/taxi.c
@@ -0,0 +1,235 @@
+#include "taxi.h"
+
+//Taxi
+Taxi* addelem_t(Taxi *head, int id, int x, int y, int sock, int status) {
+    Taxi *temp, *p;
+    do
+        temp = malloc(sizeof(Taxi));
+    while(temp == NULL);
+    temp->id = id;
+    temp->x = x;
+    temp->y = y;
+    temp->ptr = NULL;
+    temp->sock = sock;
+    temp->status = status;
+    temp->clid = -1;
+    if(head == NULL)
+        return temp;
+    p = head;
+    while(p->ptr !=NULL)
+        p = p->ptr;
+    p->ptr = temp;
+    return head;
+}
+
+Taxi* removel_t(Taxi *head, int sock) {
+    Taxi *tmp = head, *p = NULL;
+    if (head == NULL)
+        return NULL;
+    while (tmp->sock != sock) {
+        p = tmp;
+        tmp = tmp->ptr;
+    }
+    if (tmp == head) {
+        free(head);
+        return tmp->ptr;
+    }
+    p->ptr = tmp->ptr;
+    free(tmp);
+    return head;
+}
+
+Taxi* search_t(Taxi *taxis, int id) {
+    while(taxis != NULL) {
+        if(taxis->id == id) {
+            return taxis;
+        }
+        taxis = taxis->ptr;
+    }
+    return NULL;
+}
+
+Taxi* search_t_scok(Taxi *taxis, int sock) {
+    while(taxis != NULL) {
+        if(taxis->sock == sock) {
+            return taxis;
+        }
+        taxis = taxis->ptr;
+    }
+    return NULL;
+}
+
+Taxi* remove_all_t(Taxi *head) {
+    while (head != NULL) {
+        Taxi *p = head;
+        head = head->ptr;
+        free(p);
+    }
+    return NULL;
+}
+
+//Client
+Client* addelem_c(Client *head, int id, int x1, int y1, int x2, int y2, int sock, int status) {
+    Client *temp, *p;
+    do
+        temp = malloc(sizeof(Client));
+    while(temp == NULL);
+    temp->id = id;
+    temp->x1 = x1;
+    temp->y1 = y1;
+    temp->x2 = x2;
+    temp->y2 = y2;
+    temp->ptr = NULL;
+    temp->sock = sock;
+    temp->taxid = -1;
+    temp->status = status;
+    if(head == NULL) {
+        return temp;
+    }
+
+    p = head;
+    while(p->ptr !=NULL)
+        p = p->ptr;
+    p->ptr = temp;
+    return head;
+}
+
+Client* removel_c(Client *head, int sock) {
+    Client *tmp = head, *p = NULL;
+    if (head == NULL)
+        return NULL;
+    while (tmp->sock != sock) {
+        p = tmp;
+        tmp = tmp->ptr;
+    }
+    if (tmp == head) {
+        free(head);
+        return tmp->ptr;
+    }
+    p->ptr = tmp->ptr;
+    free(tmp);
+    return head;
+}
+
+Client* remove_all_c(Client *head) {
+    while (head != NULL) {
+        Client *p = head;
+        head = head->ptr;
+        free(p);
+    }
+    return NULL;
+}
+
+Client* search_c_id(Client *clients, int id) {
+    while(clients != NULL) {
+        if(clients->id == id) {
+            return clients;
+        }
+        clients = clients->ptr;
+    }
+    return NULL;
+}
+
+//General
+double pif(int x1, int y1, int x, int y) {
+    int dx = abs(x1 - x);
+    int dy = abs(y1 - y);
+    return sqrt(dx * dx + dy * dy)*RANDOMC;
+}
+
+int gettok(char* token) {
+    token = strtok(token," ");
+    return atoi(token);
+}
+
+char* getname(char* token) {
+    char* tok = strtok(token," ");
+    return tok;
+}
+
+int testpoints(int x, int y) {
+    if(x <= 0 || x > MAPSIZE || y <= 0 || y > MAPSIZE)
+        return 1;
+    else
+        return 0;
+}
+
+int search_taxi(Taxi *t, int sock) {
+    while(t != NULL) {
+        if(t->sock == sock)
+            return 1;
+        t = t->ptr;
+    }
+    return 0;
+}
+
+int search_cli(Client *t, int sock) {
+    while(t != NULL) {
+        if(t->sock == sock)
+            return 1;
+        t = t->ptr;
+    }
+    return 0;
+}
+
+char* getstatus(int status) {
+    if(status == 0)
+        return "Wait";
+    else if (status == 1)
+        return "Going to client";
+    else if (status == 2)
+        return "Waiting for client";
+    else if (status == 3)
+        return "On race";
+    else if (status == 4)
+        return "Exiting";
+    else if (status == 10)
+        return "Wait taxi";
+    else
+        return "Complete";
+}
+
+//To thread
+Res* addelem_res(Res *head, int idtax, double dist) {
+    Res *temp, *p;
+    do
+        temp = malloc(sizeof(Res));
+    while(temp == NULL);
+    temp->idtax = idtax;
+    temp->dist = dist;
+    temp->ptr = NULL;
+    if(head == NULL){
+		return temp;
+	}
+    p = head;
+    while(p->ptr !=NULL)
+        p = p->ptr;
+    p->ptr = temp;
+    return head;
+}
+
+Res* remove_all_r(Res *head) {
+    while (head != NULL) {
+        Res *p = head;
+        head = head->ptr;
+        free(p);
+    }
+    return NULL;
+}
+
+void send_stat(int sock, int status, int clid) {
+    char buff[SIZE];
+    memset(buff, '\0', sizeof(buff));
+    snprintf (buff, sizeof(buff), "%d %d %d\n", STAT, status, clid);
+    send(sock, buff, strlen(buff), 0);
+}
+
+void send_points(int sock, int x1, int y1, int x2, int y2, int status, int clid) {
+    char buff[SIZE];
+    memset(buff, '\0', sizeof(buff));
+    snprintf (buff, sizeof(buff), "%d %d %d %d %d %d %d\n",
+              STAT, status, x1, y1, x2, y2, clid);
+    if((send(sock, buff, strlen(buff), 0)) == -1) {
+        perror("send()");
+    }
+}
---
+++ C/taxiProject/taxist.c
@@ -0,0 +1,142 @@
+#include "taxi.h"
+
+void get(char *tokcopy) {
+    tokcopy = strtok (tokcopy, " ");
+    while (tokcopy != NULL) {
+        printf ("%s\n",tokcopy);
+        fflush(stdout);
+        tokcopy = strtok (NULL," ");
+    }
+}
+
+int test_sock(int sockfd, fd_set fds, struct timeval tv) {
+    if(select(sockfd + 1, &fds, NULL, NULL, &tv)) {
+        printf("Server is down!\n");
+        fflush(stdout);
+        return -1;
+    }
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    struct taxi t;
+    int n;
+    int id = getpid();
+    srand(id);
+    if(argc < 2) {
+        printf("Usage <flag 0[with generate params]|1[for enter params]> [x] [y]\n");
+        exit(-1);
+    }
+    if(atoi(argv[1]) == 0) {
+        t.id = id;
+        t.x = rand()%MAPSIZE;
+        t.y = rand()%MAPSIZE;
+    } else {
+        if(!testpoints(atoi(argv[2]), atoi(argv[3]))) {
+            t.id = id;
+            t.x = atoi(argv[2]);
+            t.y = atoi(argv[3]);
+        } else {
+            printf("Points out of range from 0 to %d\n", MAPSIZE);
+            exit(-1);
+        }
+    }
+    printf("Enter ID: %d\nx: %d\ny: %d\n",
+           t.id, t.x, t.y);
+
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    char buff[SIZE];
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("socket()\n");
+        exit(-1);
+    }
+    fd_set fds; 
+    FD_ZERO(&fds);
+    FD_SET(sockfd, &fds);
+    struct timeval tv = {0, WTIME};
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_addr.s_addr = inet_addr(ADDR);
+    int i = 1;
+    setsockopt(sockfd, IPPROTO_TCP, TCP_QUICKACK, (void *)&i, sizeof(i));
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) {
+        perror("connect()\n");
+        exit(-1);
+    }
+    int tok;
+    while(1) {
+        memset(buff, '\0', sizeof(buff));
+        if((n = recv(sockfd, &buff[0], sizeof(buff), 0)) > 0) {
+            tok = gettok(buff);
+            if(tok == CONNECT) {
+                t.status = WAIT;
+                snprintf (buff, sizeof(buff), "taxi %d %d %d %d",
+                          t.id, t.x, t.y, t.status);
+                send(sockfd, buff, strlen(buff), 0);
+                printf("New status is %s\n", getstatus(t.status));
+                fflush(stdout);
+            }
+
+            else if(tok == STAT) {
+                if(gettok(NULL) == ORD) {
+                    t.status = TO_CLI;
+                    int x1 = gettok(NULL);
+                    int y1 = gettok(NULL);
+                    int x2 = gettok(NULL);
+                    int y2 = gettok(NULL);
+                    int clid = gettok(NULL);
+                    sleep(READORDER);
+                    send_stat(sockfd, t.status, clid);
+                    printf("New status is %s %d\n", getstatus(t.status), clid);
+                    double wtime = pif(x1, y1, t.x, t.y);
+                    int percent = rand()%30;
+                    wtime += wtime + wtime/100*percent;
+                    printf("Time of race is %f\n", wtime);
+                    sleep(wtime/20);
+                    t.x = x1;
+                    t.y = y1;
+                    t.status = WAIT_CLI;
+                    if(!test_sock(sockfd, fds, tv)){
+                        break;
+                    }
+                    send_points(sockfd, x1, y1, 0, 0, t.status, clid);
+                    memset(buff, '\0', sizeof(buff));
+                    recv(sockfd, &buff[0], sizeof(buff), 0);
+                    printf("New status is %s %d and points x=%d y=%d\n", getstatus(t.status), clid, t.x, t.y);
+                    sleep(WAITINGTIME);
+                    t.status = ON_RACE;
+                    send_stat(sockfd, t.status, clid);
+                    printf("New status is %s with client %d\n", getstatus(t.status), clid);
+                    wtime = pif(x2, y2, x1, y1);
+                    percent = rand()%30;
+                    wtime += wtime + wtime/100*percent;
+                    printf("Time of race is %f\n", wtime);
+                    sleep(wtime/20);
+                    t.x = x2;
+                    t.y = y2;
+                    t.status = COMPLETE;
+                    printf("New status is %s\n", getstatus(t.status));
+                    if(!test_sock(sockfd, fds, tv)){
+                        break;
+                    }
+                    send_points(sockfd, x2, y2, 0, 0, t.status, clid);
+                    recv(sockfd, &buff[0], sizeof(buff), 0);
+                    sleep(EXITINGTIME);
+                    t.status = WAIT;
+                    printf("New status is %s\n", getstatus(t.status));
+                    send_stat(sockfd, t.status, clid);
+                } else {
+                    send_stat(sockfd, ABORT, 0);
+                }
+
+            }
+        } else {
+            printf("Server is down!\n");
+            break;
+        }
+    }
+    close(sockfd);
+    return 0;
+}
